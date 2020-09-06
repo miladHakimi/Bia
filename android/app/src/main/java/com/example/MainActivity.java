aafe6a89e3a9586cd8a34840a4e9382c233369cb
miladhakimi <miladhakimi33@gmail.com>
2020-09-06 13:06:25 +0430

    appcenter scripts added.

--- android/app/src/main/java/com/example/MainActivity.java
+++ android/app/src/main/java/com/example/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent; // <-- include if not already there
 // import com.tkporter.sendsms.SendSMSPackage;
 import android.os.Bundle;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -18,6 +22,8 @@ public class MainActivity extends ReactActivity {
     super.onCreate(savedInstanceState);
     // SplashScreen.show(...) has to be called after super.onCreate(...)
     // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
+    AppCenter.start(getApplication(), "741f3860-913a-47d6-8dc0-6267c5af3220",
+                  Analytics.class, Crashes.class);
     SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, false);
   }
 
