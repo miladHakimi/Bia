a15a1d64347802779623700f5eb9c04efa4c7456
miladhakimi <miladhakimi33@gmail.com>
2020-09-06 13:13:09 +0430

    unimodules changed

--- App.js
+++ App.js
@@ -128,23 +128,3 @@ export default function App () {
 		</Provider>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-	  flex: 1,
-	  justifyContent: "center",
-	  alignItems: "center",
-	  backgroundColor: "#F5FCFF"
-	},
-	welcome: {
-	  fontSize: 20,
-	  textAlign: "center",
-	  margin: 10
-	},
-	instructions: {
-	  fontSize: 12,
-	  textAlign: "left",
-	  color: "#333333",
-	  marginBottom: 5
-	}
-  });
